b24cb0aabf83f5257e5a5f53a60c33c8a67b21e5
jamwil123 <james789111@gmail.com>
2022-03-04 13:59:03 +0000

    added a lot of functionality

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,18 +2,41 @@ import React, { useState, useEffect} from "react";
 import Squares from './Squares.js'
 import Keyboard from './Keyboard.js'
 import squaresData from '../utils/squaresData.json'
+import dictionary from "../utils/dictionary.json"
 
 export default function Home() {
-
+  // State that changes the grids values 
   const [gridLetters, setGridLetters ] = useState(squaresData);
+  // State if player has won or not
+  const [haveWon, setHaveWon] = useState(false);
+  // State that holds the word of the day 
+  const [wordToGuess, setWordToGuess] = useState('word');
+
+  useEffect(() => {
+    randomWordSelector()
+  }, [])
   
+  function randomWordSelector() {
+    let numberOfWords = dictionary.length
+    let randomValue = (Math.random() * numberOfWords)
+    console.log(numberOfWords)
+    console.log("In here again")
+    dictionary.map((word, i)=>{
+      if(i == Math.round(randomValue))
+      {
+        console.log(word)
+        return setWordToGuess(word)
+      }
+    })
+  }
 
   return (
     <div className='mainHome'>
     <div className='title'>BAD WORDS ONLY WORDLE</div>
+    {haveWon ? <div>YOU WIN BITCH!</div> : <div></div> }
     <Squares gridLetters={gridLetters} />
 
-    <Keyboard gridLetters={gridLetters} setGridLetters={setGridLetters}/>
+    <Keyboard gridLetters={gridLetters} setGridLetters={setGridLetters} haveWon={haveWon} setHaveWon={setHaveWon} wordToGuess={wordToGuess} setWordToGuess={setWordToGuess}/>
 
     </div>
   )
--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -1,19 +1,20 @@
 import React, { useState } from "react";
 import keyboard from "../utils/keyboard.json";
+import dictionary from "../utils/dictionary.json";
 
-export default function Keyboard({ gridLetters, setGridLetters }) {
+export default function Keyboard({ gridLetters, setGridLetters, haveWon, setHaveWon, wordToGuess, setWordToGuess}) {
   const [selectedLetters, setSelectedLetters] = useState([]);
   // Which row of grid you are on
   const [gridTurnCount, setGridTurnCount] = useState(0);
   // Which square is selected
   const [gridPossition, setGridPossition] = useState(0);
-
+  
 
   //function to set our state for the letter selected by the user
   function clickedLetter(letter) {
     if (gridPossition >= 5) {
       return;
-    } // checks to see if the position is more than or equal to 5 to prevent over-reach into empty array 
+    } // checks to see if the position is more than or equal to 5 to prevent over-reach into empty array
     setGridLetters((prev) => {
       let arr = [...prev];
       arr[gridTurnCount][gridPossition] = `${letter}`;
@@ -23,15 +24,13 @@ export default function Keyboard({ gridLetters, setGridLetters }) {
 
     setGridPossition((prev) => {
       return prev + 1;
-    }); // move to the next grid position 
+    }); // move to the next grid position
   }
-
-
 
   function delAndEnter(letter) {
     if (letter === "delete") {
-      if(gridPossition === 0){
-        return
+      if (gridPossition === 0) {
+        return;
       }
       setGridLetters((prev) => {
         let arr = [...prev];
@@ -45,18 +44,40 @@ export default function Keyboard({ gridLetters, setGridLetters }) {
     }
 
     if (letter === "enter") {
-      enterButton()
+      enterButton();
     }
+  }
 
-    function enterButton() {
-      if (gridTurnCount === 5) {
-        return 0;
+  function enterButton() {
+    if (gridTurnCount === 5 || gridPossition === 0) {
+      return 0;
+    } // checks to see if player is at the end of the grids. Stops users overfilling.
+
+    // check to see if 5 words have been selected
+    if (gridPossition >= 5) {
+      let str = "";
+      gridLetters[gridTurnCount].map((letter) => {
+        str += letter;
+      });
+
+      if (wordToGuess === str) {
+        setHaveWon(true)
       }
-      setGridTurnCount((prev) => prev + 1);
-      setGridPossition(0);
-      console.log(gridPossition, gridTurnCount);
+
+      dictionary.map((word) => {
+        
+
+        if(word !== str) 
+        {
+          
+        }
+      })
+
 
     }
+
+    setGridTurnCount((prev) => prev + 1); // change grid to next one
+    setGridPossition(0); // change square back to zero
   }
 
   return (
@@ -83,6 +104,7 @@ export default function Keyboard({ gridLetters, setGridLetters }) {
               className="keyboardLetters"
               onClick={() => {
                 clickedLetter(letter);
+                console.log(this.className)
               }}
             >
               {letter}{" "}
@@ -104,7 +126,16 @@ export default function Keyboard({ gridLetters, setGridLetters }) {
               </div>
             );
           }
-          return <div className="keyboardLetters">{letter}</div>;
+          return (
+            <div
+              className="keyboardLetters"
+              onClick={() => {
+                clickedLetter(letter);
+              }}
+            >
+              {letter}
+            </div>
+          );
         })}
       </div>
     </div>
